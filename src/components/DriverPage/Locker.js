5fdb8cfa31a856d3472a8782f23bafad26a19fec
GPU-Fried-Eggs <seele.peng@gmail.com>
2023-11-15 15:26:14 +0200

    Add fetch api

--- src/components/DriverPage/Locker.js
+++ src/components/DriverPage/Locker.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Cabinet from './Cabinet';
 import styles from './Locker.module.css';
 
-function Locker({ cabinets, onCabinetClick }) {
+function Locker({ id, onCabinetClick }) {
+    const [lockerData, setLockerData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchLockerData() {
+            try {
+                const response = await fetch(`${process.env.REACT_APP_API}/locker/${id}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setLockerData(data);
+                } else {
+                    setError(`HTTP error! Status: ${response.status}`);
+                }
+            } catch (error) {
+                console.error('Error fetching locker data:', error);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchLockerData().then();
+    }, [id]);
+
+    if (isLoading) {
+        return (
+            <div className="loading">
+                <div className="loading-animation"></div>
+                Loading...
+            </div>
+        );
+    }
+
+    if (error) {
+        return <div className="error">Error: {error}</div>;
+    }
+
     return (
-        <div className={styles.locker}>
-            {cabinets.map((cabinet, index) => (
-                <Cabinet
-                    key={index}
-                    isOccupied={cabinet.isLocked}
-                    onClick={() => onCabinetClick(cabinet.id)}
-                />
-            ))}
-        </div>
+        <>
+            <h2>Locker Location: {lockerData.location}</h2>
+            <div className={styles.locker}>
+                {lockerData.cabinets.map((cabinet, index) => (
+                    <Cabinet
+                        key={index}
+                        isOccupied={cabinet.isLocked}
+                        onClick={() => onCabinetClick(cabinet.id)}
+                    />
+                ))}
+            </div>
+        </>
     )
 }
 
