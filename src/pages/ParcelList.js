a582fa0c4c186469c282155bfea1539c6b40e676
GPU-Fried-Eggs <seele.peng@gmail.com>
2023-12-08 23:51:32 +0200

    remove unused check

--- src/pages/ParcelList.js
+++ src/pages/ParcelList.js
@@ -1,22 +1,9 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Sidebar from "../components/UserPage/Sidebar";
 import ParcelCard from "../components/UserPage/ParcelCard";
 import "./ParcelList.css";
 
 export default function ParcelList() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const loggedInUser = localStorage.getItem("authenticated");
-    const isUserAuthenticated = loggedInUser === "true";
-
-    if (!isUserAuthenticated) {
-      navigate("/alchemists/login");
-    }
-  }, [navigate]);
-
   return (
     <>
       <header>
